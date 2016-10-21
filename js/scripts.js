058a3e3197f9240fdbc3a1e83680b86776deb68e
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-21 14:02:55 -0700

    modify index.html, css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,20 @@ var company = $("input:radio[name=company]:checked").val(); //Company Radio
 
 var size = $("input:radio[name=size]:checked").val(); //Size Radio
 
-var build = $("input:radio[name=build]:checked").val(); //Build Radio
+//var build = $("input:radio[name=build]:checked").val(); //Build Radio
 
 //Checkboxes off
 
-//Drop-down off 
+//Drop-down off
 
 });
 
 // User input:
 $(document).ready(function() {
+  $("form#survey").submit(function(event) {
+
+    
+
+    event.preventDefault();
+  });
+});
