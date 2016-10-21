4af727b0fb43713d4f704400a12496f73de845bc
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-21 15:48:26 -0700

    add Bootstrap dropdown menu but it doesn't function. add css styling by changing font size, margins and colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 // Business logic:
+  var company = $("input:radio[name=company]:checked").val();
 
-// var company = $("input:radio[name=company]:checked").val(); //Company Radio
-
-//var size = $("input:radio[name=size]:checked").val(); //Size Radio
+  //var size = $("input:radio[name=size]:checked").val();
 
 //var build = $("input:radio[name=build]:checked").val(); //Build Radio
 
@@ -10,24 +9,36 @@
 
 //Drop-down off
 
-
 // User input:
-$(function() {
+$(document).ready(function() {
+  //$('.dropdown-toggle').dropdown(); //activiates Drop-down 
   $("form").submit(function(event) {
-    var company = $("input:radio[name=company]:checked").val();
 
-    if ( company1 === 'option1' ) {
+    if ( company === 'option1' ) {
       $( "#output1" ).show(); // C# and .NET
     }
-    else if ( company2 === 'option2' ) {
+    else if ( company === 'option2' ) {
       $( "output2" ).show(); // Java and Android
     }
-    else if ( company3 === 'option 3' ) {
+    else if ( company === 'option3' ) {
       $( "output3" ).show(); // CSS and Design
     }
     else {
       alert( "Please select something" );
     }
+
+    //if ( size === 'option4' || 'option5') {
+    //  $( "#output1" ).show(); // C# and .NET OR Java
+    //}
+    //else if ( size === 'option5' || 'option6' ) {
+    //  $( "output2" ).show(); // Java and Android OR CSS
+    //}
+    //else if ( size === 'option6' ) {
+    //  $( "output3" ).show(); // CSS and Design
+    //}
+    //  else {
+    //  alert( "Please select something" );
+    //}
     event.preventDefault();
   });
 });
