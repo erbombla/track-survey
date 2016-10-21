7689438146a9cbc4527b6241df05faf14dae9063
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-21 14:55:59 -0700

    added if/else if statements to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Business logic:
 
-var company = $("input:radio[name=company]:checked").val(); //Company Radio
+// var company = $("input:radio[name=company]:checked").val(); //Company Radio
 
-var size = $("input:radio[name=size]:checked").val(); //Size Radio
+//var size = $("input:radio[name=size]:checked").val(); //Size Radio
 
 //var build = $("input:radio[name=build]:checked").val(); //Build Radio
 
@@ -10,14 +10,24 @@ var size = $("input:radio[name=size]:checked").val(); //Size Radio
 
 //Drop-down off
 
-});
 
 // User input:
-$(document).ready(function() {
-  $("form#survey").submit(function(event) {
+$(function() {
+  $("form").submit(function(event) {
+    var company = $("input:radio[name=company]:checked").val();
 
-    
-
+    if ( company1 === 'option1' ) {
+      $( "#output1" ).show(); // C# and .NET
+    }
+    else if ( company2 === 'option2' ) {
+      $( "output2" ).show(); // Java and Android
+    }
+    else if ( company3 === 'option 3' ) {
+      $( "output3" ).show(); // CSS and Design
+    }
+    else {
+      alert( "Please select something" );
+    }
     event.preventDefault();
   });
 });
