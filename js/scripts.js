47b4b0c369d4034f7efbbbc25688fd1e33ca3e3a
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-21 11:34:58 -0700

    initialize JS/jQuery scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
 // Business (or back-end) logic:
 
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
 
 // User input:
 $(document).ready(function() {
