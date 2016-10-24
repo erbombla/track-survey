8505499276cd06d02bc0252114a047a5b947534c
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-23 19:49:38 -0700

    work on JS script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
+// User input:
+$(document).ready(function() {
+  $('.dropdown-toggle').dropdown();
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+      var company = $("input:radio[name=company]:checked").val();
+
 // Business logic:
-var company = $("input:radio[name=company]:checked").val();
+var survey = function(){
+  if ( company === 'option1' ) {
+    return( "#output1" ); // C# and .NET
+  }
+  else if ( company === 'option2' ) {
+    return( "output2" ); // Java and Android
+  }
+  if else ( company === 'option3' ) {
+    return( "output3" ); // CSS and Design
+  }
+  else {
+    alert( "Please select something" );
+  }
+};
+
+//if ( size === 'option4' || 'option5') {
+//  $( "#output1" ).show(); // C# and .NET OR Java
+//}
+//else if ( size === 'option5' || 'option6' ) {
+//  $( "output2" ).show(); // Java and Android OR CSS
+//}
+//else if ( size === 'option6' ) {
+//  $( "output3" ).show(); // CSS and Design
+//}
+//  else {
+//  alert( "Please select something" );
+//}
 
 //var size = $("input:radio[name=size]:checked").val();
 
@@ -8,36 +41,3 @@ var company = $("input:radio[name=company]:checked").val();
 //Checkboxes off
 
 //Drop-down off
-
-// User input:
-$(document).ready(function() {
-  //$('.dropdown-toggle').dropdown(); //activiates Drop-down
-  $("form#survey").submit(function(event) {
-    if ( company === 'option1' ) {
-      $( "#output1" ).show(); // C# and .NET
-    }
-    if else ( company === 'option2' ) {
-      $( "output2" ).show(); // Java and Android
-    }
-    if else ( company === 'option3' ) {
-      $( "output3" ).show(); // CSS and Design
-    }
-    else {
-      alert( "Please select something" );
-    }
-
-    //if ( size === 'option4' || 'option5') {
-    //  $( "#output1" ).show(); // C# and .NET OR Java
-    //}
-    //else if ( size === 'option5' || 'option6' ) {
-    //  $( "output2" ).show(); // Java and Android OR CSS
-    //}
-    //else if ( size === 'option6' ) {
-    //  $( "output3" ).show(); // CSS and Design
-    //}
-    //  else {
-    //  alert( "Please select something" );
-    //}
-    event.preventDefault();
-  });
-});
